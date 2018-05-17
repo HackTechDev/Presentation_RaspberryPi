f84fb62ff11b0f094f4d1db33fafc4578f856e72
samuel.gondouin <samuel.gondouin@gandi.net>
2018-05-17 13:27:35 +0200

    Merge https://github.com/Nekrofage/prezraspberry

---
+++ sponsor.php
@@ -0,0 +1,23 @@
+                <section>
+                    <section>
+                        <h1>Sponsors</h1>
+                    </section>
+
+                    <section>
+                        <h1></h1>
+                        <a href="http://eurodns.com"><img src="image/eurodns.png" width="80%"></a>
+                    </section>
+
+
+                    <section>
+                        <h1></h1>
+                        <a href="http://www.hackable.fr/"><img src="image/hackable.jpg" width="80%"></a>
+                    </section>
+
+                    <section>
+                        <h1></h1>
+                        <a href="http://www.electroniquepratique.com/"><img src="image/electroniquepratique.jpg" width="80%"></a>
+                    </section>
+
+                </section>
+
